ef8f8feb17ed16346a87a31859356dfd4d534e8f
ysk8hori <ysk8.hori@gmail.com>
2022-04-18 21:42:20 +0900

    index.ts から GameType を export

--- src/index.ts
+++ src/index.ts
@@ -14,6 +14,7 @@ import BaseHeight from './core/valueobject/baseHeight';
 import BaseWidth from './core/valueobject/baseWidth';
 import GameID from './core/valueobject/gameId';
 import { GameType } from './core/types';
+export { GameType } from './core/types';
 
 const cellRepository = CellRepositoryImpl.create();
 const groupRepository = GroupRepositoryImpl.create();
